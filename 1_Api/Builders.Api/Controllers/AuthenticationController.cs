6890c89255d11cf83bb3da2145abec5b71665a82
marcus.barbosa <marcus.barbosa@simplevisionit.com>
2021-08-13 22:59:57 -0300

    Organizando a controlller

--- 1_Api/Builders.Api/Controllers/AuthenticationController.cs
+++ 1_Api/Builders.Api/Controllers/AuthenticationController.cs
@@ -5,7 +5,6 @@ using Builders.Shared.Commands;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Threading.Tasks;
 
 namespace Builders.Api.Controllers
@@ -18,29 +17,19 @@ namespace Builders.Api.Controllers
         [AllowAnonymous]
         public async Task<ICommandResult> Post([FromBody] AuthenticationCommand command)
         {
-            try
-            {
-                command.Validate();
-                if (!command.Valid)
-                    return new CommandResult(false, "Erros", command.Notifications);
+            command.Validate();
+            if (!command.Valid)
+                return new CommandResult(false, "Erros", command.Notifications);
 
-                if (command.UserName == "Admin" && command.PassWord == "123456")
-                {
-                    var token = TokenService.GenerateToken(command.UserName);
-                    return new CommandResult(true, "", new
-                    {
-                        token = "Bearer " + token
-                    });
-                }
-                else
-                {
-                    return new CommandResult(false, "404", StatusCodes.Status404NotFound);
-                }
-            }
-            catch (Exception ex)
+            if (command.UserName == "Admin" && command.PassWord == "123456")
             {
-                return new CommandResult(false, ex.Message, StatusCodes.Status500InternalServerError);
+                var token = TokenService.GenerateToken(command.UserName);
+                return new CommandResult(true, "", new
+                {
+                    token = "Bearer " + token
+                });
             }
+            return new CommandResult(false, "404", StatusCodes.Status404NotFound);
         }
     }
 }
